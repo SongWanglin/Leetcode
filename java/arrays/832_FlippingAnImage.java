ef113149898bf42af6e149ab5817b37f1e5a64a9
WanglinSong <wsong14@hawk.iit.edu>
2018-12-20 00:00:30 -0600

    832 ez

--- java/arrays/832_FlippingAnImage.java
+++ java/arrays/832_FlippingAnImage.java
@@ -1,5 +1,7 @@
 class Solution {
     public int[][] flipAndInvertImage(int[][] A) {
+        if (A==null || A.length==0 )
+            return new int[0][0];
         int row = A.length;
         int col = A[0].length;
         int[][] B = new int[row][col];
