99efbd1531b8239f1fc12e284a65a0cb57918055
WanglinSong <wsong14@hawk.iit.edu>
2018-12-25 00:01:14 -0600

    ReturnOrigin(SecondAttempt:iterate through the string)

--- java/string/657_RobotReturnOrigin.java
+++ java/string/657_RobotReturnOrigin.java
@@ -1,10 +1,14 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        return count(moves,"U") == count(moves, "D") &&
-            count(moves,"L") == count(moves,"R");
-    }
-
-    public int count(String moves, String s){
-        return moves.length() - moves.replaceAll(s,"").length();
+        int x = 0, y = 0;
+        for (int i = 0; i<moves.length(); i++){
+            switch(moves.charAt(i)){
+                case 'U': y++; break;
+                case 'D': y--; break;
+                case 'L': x--; break;
+                case 'R': x++; break;
+            }
+        }
+        return x==0 && y==0;
     }
 }
