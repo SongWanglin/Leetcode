4945caf96d69528026d2b8fa29f2c02a269cf476
WanglinSong <wsong14@hawk.iit.edu>
2019-01-08 19:34:07 -0600

    pancake sorting (greedy but not optimal)

--- python/arrays/766_ToeplitzMatrix.py
+++ python/arrays/766_ToeplitzMatrix.py
@@ -4,8 +4,8 @@ class Solution:
         :type matrix: List[List[int]]
         :rtype: bool
         """
-        for i in range(len(matrix)-1):
-            for j in range(len(matrix[0])-1):
-                if matrix[i][j]!=matrix[i+1][j+1]:
+        for i in range(len(matrix) - 1):
+            for j in range(len(matrix[0]) - 1):
+                if matrix[i][j] != matrix[i + 1][j + 1]:
                     return False
         return True
