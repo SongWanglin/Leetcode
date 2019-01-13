acf711e76e2124fdd6f453d7bb679f61fc07dbab
WanglinSong <wsong14@hawk.iit.edu>
2019-01-12 22:54:41 -0600

    bfs

---
+++ java/tree/107_LevelOrderFromBottom.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> currentLevel = new ArrayList<>();
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if (root == null)
+            return res;
+        Queue<TreeNode> curlevel = new LinkedList<TreeNode>();
+        curlevel.add(root);
+        bfs(curlevel);
+        Collections.reverse(res);
+        return res;
+    }
+        private void bfs(Queue<TreeNode> current){
+            Queue<TreeNode> nextlevel = new LinkedList<TreeNode>();
+            while(!current.isEmpty()){
+                TreeNode temp = current.poll();
+                if(temp.left!=null) nextlevel.add(temp.left);
+                if(temp.right!=null) nextlevel.add(temp.right);
+                currentLevel.add(temp.val);
+                if (current.isEmpty()){
+                    res.add(currentLevel);
+                    currentLevel = new ArrayList();
+                    bfs(nextlevel);
+                }
+            }
+        }
+}
