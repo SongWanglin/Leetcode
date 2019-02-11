ab37f71c5f73cafc2b5be0d6d06cf1d03720a1a1
WanglinSong <wsong14@hawk.iit.edu>
2019-02-10 19:51:54 -0600

    path sum III: dynamic programming optimization

--- java/tree/437_PathSumIII/Solution.java
+++ java/tree/437_PathSumIII/Solution.java
@@ -8,6 +8,26 @@
  * }
  */
 class Solution {
+    /* dynamic programming approach */
+    public int pathSum(TreeNode root, int sum) {
+        int[] path = new int[1000];
+        return helper(root,sum, path,0);
+    }
+    private int helper(TreeNode root, int sum, int[] path, int level) {
+        int count = 0;
+        if (root == null) return 0;
+        if (level == 0) path[level] = root.val;
+        else path[level] = path[level - 1] + root.val;
+        if (path[level] == sum) count++;
+        for (int i = 0; i < level; i++) {
+            if (path[level] - path[i] == sum) count++;
+        }
+        count = count + helper(root.left, sum, path, level + 1);
+        count = count + helper(root.right, sum, path, level + 1);
+        return count;
+    }
+    /* intuitive recursive solution */
+    /*/
     public int pathSum(TreeNode root, int sum) {
         if(root == null){
             return 0;
@@ -21,4 +41,5 @@ class Solution {
             sum -= root.val;
             return ( (sum ==0 )?1:0 ) + dfs(root.left, sum)+dfs(root.right, sum);
         }
+        /*/
 }
