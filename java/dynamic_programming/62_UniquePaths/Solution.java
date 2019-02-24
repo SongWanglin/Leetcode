235fc76bf6c1908ff10401cae5be91ef9c4b1a49
WanglinSong <wsong14@hawk.iit.edu>
2019-02-23 19:13:26 -0600

    robot path (dynamic programmming)

---
+++ java/dynamic_programming/62_UniquePaths/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    /* recursive solution */
+    /*/
+    public int uniquePaths(int m, int n) {
+        if(m==1 || n==1)
+            return 1;
+        return uniquePaths(m-1, n) + uniquePaths(m, n-1);
+    }
+    /*/
+    public int uniquePaths(int m, int n){
+        int[][] map = new int[m][n];
+        for (int i = 0; i<m; i++){
+            map[i][0] = 1;
+        }
+        for (int j = 0; j<n; j++){
+            map[0][j] = 1;
+        }
+        for (int i = 1; i<m; i++){
+            for(int j = 1; j<n; j++){
+                map[i][j] = map[i-1][j] + map[i][j-1];
+            }
+        }
+        return map[m-1][n-1];
+    }
+}
