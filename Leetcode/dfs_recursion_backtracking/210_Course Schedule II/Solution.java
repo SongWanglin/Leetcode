5d3cdc22bd62986e5398b1e755b422b57aa58b05
WanglinSong <wsong14@hawk.iit.edu>
2019-03-27 09:21:50 -0500

    topological sort II

--- Leetcode/dfs_recursion_backtracking/210_Course Schedule II/Solution.java
+++ Leetcode/dfs_recursion_backtracking/210_Course Schedule II/Solution.java
@@ -1,26 +1,33 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
+        // adjacency list
         List<List<Integer>> adjList = new ArrayList<>();
+        // initialize and populate the adjacency list
         for(int i = 0; i<numCourses; ++i){ adjList.add(new ArrayList<Integer>());}
         for(int[] edge: prerequisites){ adjList.get(edge[1]).add(edge[0]); }
+        // visited status
         int[] status = new int[numCourses];
         LinkedList<Integer> topoSeq = new LinkedList<>();
         for(int i = 0; i<numCourses; ++i){
+            // if not visited, do dfs to see if cycle exists
             if(status[i] == 0){
                 if(isCycleExisting(topoSeq, adjList, status, i)){
                     return new int[0];
                 }
             }
         }
+        // return topological sorted list
         int[] ans= new int[topoSeq.size()];
         for(int i = 0; i<numCourses; ++i){
             ans[i] = topoSeq.get(i);
         }
         return ans;
     }
+        // visit status: 0 - unvisited, 1 - visiting, 2 - visited
         private boolean isCycleExisting(LinkedList<Integer> topoSeq, List<List<Integer>> adjList,
                                        int[] status, int cur){
             status[cur] = 1;
+            // dfs recursion, return true if we run into visited node
             for(Integer succ: adjList.get(cur)){
                 if(status[succ]==1){return true;}
                 if(status[succ]==0){
