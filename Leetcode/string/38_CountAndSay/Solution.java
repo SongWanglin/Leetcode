fe8b7013224b6783539747454a391427b40378f7
WanglinSong <wsong14@hawk.iit.edu>
2019-06-19 23:52:23 -0500

    trailing zeroes

--- Leetcode/string/38_CountAndSay/Solution.java
+++ Leetcode/string/38_CountAndSay/Solution.java
@@ -1,26 +1,25 @@
-public class Solution {
+class Solution {
     public String countAndSay(int n) {
-            StringBuilder curr=new StringBuilder("1");
-            StringBuilder prev;
-            int count;
-            char say;
-            for (int i=1;i<n;i++){
-                prev=curr;
-                curr=new StringBuilder();
-                count=1;
-                say=prev.charAt(0);
-
-                for (int j=1,len=prev.length();j<len;j++){
-                    if (prev.charAt(j)!=say){
-                        curr.append(count).append(say);
-                        count=1;
-                        say=prev.charAt(j);
-                    }
-                    else count++;
+        StringBuilder curr = new StringBuilder("1");
+        StringBuilder prev;
+        int count;
+        char say;
+        for(int i = 1; i<n; i++){
+        // two pointer tracking
+            prev = curr;
+            curr = new StringBuilder();
+            count = 1;  say = prev.charAt(0);
+            for(int j=1, len = prev.length(); j<len; j++){
+                if(prev.charAt(j)==say){
+                    count++;
+                    continue;
                 }
                 curr.append(count).append(say);
+                count = 1;
+                say = prev.charAt(j);
             }
-            return curr.toString();
-
+            curr.append(count).append(say);
+        }
+        return curr.toString();
     }
 }
