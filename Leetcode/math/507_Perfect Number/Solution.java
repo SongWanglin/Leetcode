66f0a45a9575930be7f4ec624df3b1bea66e1856
WanglinSong <wsong14@hawk.iit.edu>
2019-07-17 01:26:00 -0500

    perfect number

---
+++ Leetcode/math/507_Perfect Number/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if(num==1) return false;
+        int res = 1;
+        for(int i = 2; i <= Math.sqrt(num); i++){
+            if(num%i!=0){
+                continue;
+            }
+            res += i + num/i;
+        }
+        return res == num;
+    }
+}
