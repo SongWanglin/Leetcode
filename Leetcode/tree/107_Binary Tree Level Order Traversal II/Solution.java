a2bbde7b64056089c8a16209921b2bc5d29957eb
WanglinSong <wsong14@hawk.iit.edu>
2019-07-31 17:41:00 -0500

    add more concise solution for level order traversal II

--- Leetcode/tree/107_Binary Tree Level Order Traversal II/Solution.java
+++ Leetcode/tree/107_Binary Tree Level Order Traversal II/Solution.java
@@ -34,3 +34,20 @@ class Solution {
             }
         }
 }
+/*/
+        public List<List<Integer>> levelOrderBottom(TreeNode root) {
+            List<List<Integer>> res = new LinkedList<List<Integer>>();
+            levelMaker(res, root, 0);
+            return res;
+        }
+
+            public void levelMaker(List<List<Integer>> list, TreeNode root, int level) {
+                if(root == null) return;
+                if(level >= list.size()) {
+                    list.add(0, new LinkedList<Integer>());
+                }
+                levelMaker(list, root.left, level+1);
+                levelMaker(list, root.right, level+1);
+                list.get(list.size()-level-1).add(root.val);
+            }
+/*/
