8c3aca85d0b1d703467c7243af2955d57c2fe847
WanglinSong <wsong14@hawk.iit.edu>
2019-08-06 22:02:48 -0500

    preorder & post order

--- Leetcode/tree/144_BinaryTreePreorderTraversal/Solution.java
+++ Leetcode/tree/144_BinaryTreePreorderTraversal/Solution.java
@@ -1,26 +1,55 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
 class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<Integer>();
-        Stack<TreeNode> stk = new Stack<>();
-        stk.add(root);
-        while(!stk.isEmpty()){
-            TreeNode temp = stk.pop();
-            if(temp==null){
-                continue;
-            }
-            res.add(temp.val);
-            stk.push(temp.right);
-            stk.push(temp.left);
-        }
+     //recursive solution
+    public List<Integer> preorder(Node root) {
+        List<Integer> res = new LinkedList<>();
+        preorderTraversal(res, root);
         return res;
     }
+        private void preorderTraversal(List<Integer> res, Node root){
+            if(root==null) return;
+            res.add(root.val);
+            for(Node child: root.children){
+                preorderTraversal(res, child);
+            }
+        }
+    // recursive solution 2
+    /*/
+    public List<Integer> preorder(Node root){
+        List<Integer> res = new LinkedList<>();
+        if(root == null)
+            return res;
+        res.add(root.val);
+        for(Node child : root.children) {
+            res.addAll(preorder(child));
+        }
+        return res;
+    }/*/
+    /*/ //iterative solution
+    public List<Integer> preorder(Node root) {
+        List<Integer> res = new LinkedList<>();
+        if (root == null) return res;
+        Stack<Node> stack = new Stack<>();
+        stack.add(root);
+        while (!stack.empty()) {
+            root = stack.pop();
+            res.add(root.val);
+            for (int i = root.children.size() - 1; i >= 0; i--)
+                stack.push(root.children.get(i));
+        }
+        return res;
+    }/*/
 }
--- Leetcode/tree/590_N-aryTreePostorderTraversal/Solution.java
+++ Leetcode/tree/590_N-aryTreePostorderTraversal/Solution.java
@@ -13,18 +13,42 @@ class Node {
 };
 */
 class Solution {
+    /*/ //recursive solution
     public List<Integer> postorder(Node root) {
         List<Integer> res = new LinkedList<>();
-        dfs(res, root);
+        preorderTraversal(res, root);
         return res;
     }
-        private void dfs(List<Integer> res, Node root){
-            if(root==null){
-                return;
-            }
-            for(Node n: root.children){
-                dfs(res, n);
+        private void preorderTraversal(List<Integer> res, Node root){
+            if(root==null) return;
+            for(Node child: root.children){
+                preorderTraversal(res, child);
             }
             res.add(root.val);
+        }/*/
+    // recursive solution2
+    public List<Integer> postorder(Node root){
+        List<Integer> res = new LinkedList<>();
+        if(root == null)
+            return res;
+        for(Node child : root.children) {
+            res.addAll(postorder(child));
         }
+        res.add(root.val);
+        return res;
+    }
+    /*/ //iterative solution
+    public List<Integer> postorder(Node root){
+        LinkedList<Integer> res = new LinkedList<>();
+        if (root == null) return res;
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.empty()) {
+            Node temp = stack.pop();
+            res.add(0, temp.val);
+            for (int i = 0; i < temp.children.size(); i++)
+                stack.push(temp.children.get(i));
+        }
+        return res;
+    }/*/
 }
