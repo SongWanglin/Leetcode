1d98201ca5a1a01838688313bb6901b0a2d24320
WanglinSong <wsong14@hawk.iit.edu>
2019-08-09 21:35:03 -0500

    middle of linkedlist

--- Leetcode/linkedlist/206_ReverseLinkedList/Solution.java
+++ Leetcode/linkedlist/206_ReverseLinkedList/Solution.java
@@ -8,10 +8,20 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
+        /*/ // recursive
         if(head==null || head.next==null) return head;
         ListNode res = reverseList(head.next);
         head.next.next = head;
         head.next = null;
+        return res;/*/
+        ListNode res = null;
+        ListNode next = null;
+        while(head!=null){
+            next = head.next;
+            head.next = res;
+            res = head;
+            head = next;
+        }
         return res;
     }
 }
