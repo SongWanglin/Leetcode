e782ffbf974068c0ee2a981e57a5773c5589f2a1
WanglinSong <wsong14@hawk.iit.edu>
2019-08-09 19:00:05 -0500

    reverse list

--- Leetcode/linkedlist/206_ReverseLinkedList/Solution.java
+++ Leetcode/linkedlist/206_ReverseLinkedList/Solution.java
@@ -8,23 +8,10 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-       /* recursive solution */
-        /*/
-        if (head == null || head.next == null)
-            return head;
-        ListNode newList = reverseList(head.next);
+        if(head==null || head.next==null) return head;
+        ListNode res = reverseList(head.next);
         head.next.next = head;
         head.next = null;
-        return newList;/*/
-        /* iterative solution */
-        ListNode next = null;
-        ListNode preHead = null;
-        while(head!=null){
-            next = head.next;
-            head.next = preHead;
-            preHead = head;
-            head = next;
-        }
-        return preHead;
+        return res;
     }
 }
