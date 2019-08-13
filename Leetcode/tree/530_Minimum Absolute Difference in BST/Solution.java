48eb5778ebbd20040d8992798655df2701073e8c
WanglinSong <wsong14@hawk.iit.edu>
2019-08-12 23:18:38 -0500

    minimum distance between bst nodes

---
+++ Leetcode/tree/530_Minimum Absolute Difference in BST/Solution.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int getMinimumDifference(TreeNode root) {
+        int min = Integer.MAX_VALUE;
+        Integer prev = null;
+        Stack<TreeNode> stk = new Stack<>();
+        TreeNode temp = root;
+        while(temp!=null || !stk.isEmpty()){
+            while(temp!=null){
+                stk.push(temp);
+                temp = temp.left;
+            }
+            temp = stk.pop();
+            if(prev!=null) min = Math.min(min, temp.val-prev);
+            prev = temp.val;
+            temp = temp.right;
+        }
+        return min;
+    }
+}
