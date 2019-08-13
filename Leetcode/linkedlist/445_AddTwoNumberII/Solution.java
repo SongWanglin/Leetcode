870a18fea9f74deddede33da6301be74fc9da0cf
WanglinSong <wsong14@hawk.iit.edu>
2019-08-12 21:11:04 -0500

    add 2 numbers with deque:

--- Leetcode/linkedlist/445_AddTwoNumberII/Solution.java
+++ Leetcode/linkedlist/445_AddTwoNumberII/Solution.java
@@ -7,29 +7,55 @@
  * }
  */
 class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+        int addOne = 0, sum = 0, length1 = 0, length2 = 0;
+        Deque<Integer> deque = new ArrayDeque<>();
+        ListNode p1 = l1, p2 = l2, res = new ListNode(0);
+        while(p1!=null){
+            deque.addFirst(p1.val);
+            p1 = p1.next;
+            length1++;
+        }
+        while(p2!=null){
+            deque.addLast(p2.val);
+            p2 = p2.next;
+            length2++;
+        }
+        while(!deque.isEmpty() || addOne!=0){
+            sum = ((length1>0)? deque.pollFirst():0) + ((length2>0)? deque.pollLast():0) +res.val;
+            res.val = sum%10;
+            addOne = sum/10;
+            ListNode pre = new ListNode(addOne);
+            pre.next = res;
+            res = pre;
+            length1--; length2--;
+        }
+        return (res.val==1)? res: res.next;
+    }
+    /*/// solution with list reversing
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int addOne = 0, digit = 0;
-        ListNode res = new ListNode(0), temp = res;
         l1 = reverseList(l1);
         l2 = reverseList(l2);
+        int addOne = 0, sum = 0;
+        ListNode dummyHead = new ListNode(-1), temp = dummyHead;
         while( l1!=null || l2!=null || addOne!=0){
-            digit = ((l1!=null)? l1.val:0) + ((l2!=null)? l2.val:0) + addOne;
-            addOne = digit/10;
-            ListNode current = new ListNode(digit%10);
-            temp.next = current;
+            sum = ((l1==null)?0:l1.val)+((l2==null)?0:l2.val)+addOne;
+            temp.next = new ListNode(sum%10);
             temp = temp.next;
+            addOne = sum/10;
             l1 = (l1==null)? l1: l1.next;
             l2 = (l2==null)? l2: l2.next;
         }
-        return reverseList(res.next);
+        ListNode res = reverseList(dummyHead.next);
+        return res;
     }
         private ListNode reverseList(ListNode head){
             if(head==null || head.next==null){
                 return head;
             }
-            ListNode newList = reverseList(head.next);
+            ListNode res = reverseList(head.next);
             head.next.next = head;
             head.next = null;
-            return newList;
-        }
+            return res;
+        }/*/
 }
