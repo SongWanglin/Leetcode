5884ded7ec105b3fd7055a48c8584ed21f6947c8
WanglinSong <wsong14@hawk.iit.edu>
2019-08-17 21:10:18 -0500

    array partition

--- Leetcode/arrays/561_ArrayPartition/Solution.java
+++ Leetcode/arrays/561_ArrayPartition/Solution.java
@@ -1,10 +1,10 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
-        int n = nums.length, sum = 0;
         Arrays.sort(nums);
-        for (int i =0; i<n/2; i++){
-            sum +=nums[2*i];
+        int res = 0;
+        for(int i = 0; i<nums.length-1; i+=2){
+            res += nums[i];
         }
-        return sum;
+        return res;
     }
 }
