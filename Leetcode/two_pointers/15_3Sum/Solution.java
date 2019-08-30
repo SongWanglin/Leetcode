577b6b506ed85206563599f2803b02cfb570b1d7
WanglinSong <wsong14@hawk.iit.edu>
2019-08-29 23:53:47 -0500

    3 sum

--- Leetcode/two_pointers/15_3Sum/Solution.java
+++ Leetcode/two_pointers/15_3Sum/Solution.java
@@ -1,24 +1,47 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
+    // waiting to be optimized
+    public List<List<Integer>> threeSum(int[] nums){
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        List<List<Integer>> res = new LinkedList<>();
         for(int i = 0; i<nums.length-2; i++){
-            if( i==0 || (i>0 && nums[i]!=nums[i-1])) {
-                int lo = i+1, hi = nums.length-1, sum = 0 - nums[i];
-                while( lo<hi ){
-                    if(nums[lo]+nums[hi]==sum){
-                        res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
-                        while(lo<hi && nums[lo]==nums[lo+1]) lo++;
-                        while(lo<hi && nums[hi]==nums[hi-1]) hi--;
-                        lo++; hi--;
-                    }else if(nums[lo]+nums[hi]<sum){
-                        lo++;
-                    }else{
-                        hi--;
-                    }
+            if(i>0 && nums[i]==nums[i-1])
+                continue;
+            int start = i+1, end = nums.length-1;
+            while(start<end){
+                int a = nums[start], b = nums[end];
+                if(a+b+nums[i]==0){
+                    List<Integer> temp = Arrays.asList(a, b, nums[i]);
+                    res.add(temp);
+                    while(start<end && nums[start]==nums[start+1]) start++;
+                    while(start<end && nums[end]==nums[end-1]) end--;
+                }
+                if(a+b+nums[i]>0){
+                    end-=1;
+                }else{
+                    start+=1;
                 }
             }
         }
         return res;
     }
+
+     /*///brute force, TLE
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i = 0; i<nums.length-2; i++){
+            Set<Integer> set = new HashSet<>();
+            for(int j = i+1; j<nums.length; j++){
+                if(set.contains(-nums[i]-nums[j])){
+                    int[] arr = {nums[i], -nums[i]-nums[j], nums[j]};
+                    Arrays.sort(arr);
+                    List<Integer> temp = new LinkedList<>();
+                    for(int num: arr) temp.add(num);
+                    if(!res.contains(temp))
+                        res.add(temp);
+                }
+                set.add(nums[j]);
+            }
+        }
+        return res;
+    }/*/
 }
--- Leetcode/two_pointers/15_3Sum/Solution.py
+++ Leetcode/two_pointers/15_3Sum/Solution.py
@@ -1,22 +1,27 @@
-class Solution:
+class Solution(object):
     def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
         res = []
         nums.sort()
-
-        for i in range(len(nums)-1):
-            if i > 0 and nums[i] == nums[i-1]:
+        for i in xrange(len(nums)-2):
+            if i>0 and nums[i]==nums[i-1]:
                 continue
-            if nums[i] > 0:
-                break
-            d = {}
-            j = i + 1
-            while j < len(nums):
-                req = -nums[i] - nums[j]
-                if req in d:
-                    res.append([nums[i], req, nums[j]])
-                    while j < len(nums)-1 and nums[j] == nums[j+1]:
-                        j = j + 1
+            l, r = i+1, len(nums)-1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l+=1
+                elif s > 0:
+                    r-=1
                 else:
-                    d[nums[j]] = j
-                j += 1
+                    res.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
         return res
