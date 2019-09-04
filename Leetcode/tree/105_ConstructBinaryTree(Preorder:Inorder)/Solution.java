0f3f51d47d4b960679a96c343382ae9cc131e9f3
WanglinSong <wsong14@hawk.iit.edu>
2019-09-03 21:26:20 -0500

    construct binary tree

--- Leetcode/tree/105_ConstructBinaryTree(Preorder:Inorder)/Solution.java
+++ Leetcode/tree/105_ConstructBinaryTree(Preorder:Inorder)/Solution.java
@@ -9,21 +9,22 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return helper(0, 0, inorder.length-1, preorder, inorder);
+        int[] start = new int[1];
+        return dfs(preorder, start, inorder, 0, inorder.length-1);
     }
-        private TreeNode helper(int preStart, int inStart, int inEnd,
-                               int[] preorder, int[] inorder){
-            if (inStart>inEnd || preStart >= preorder.length){
+        private TreeNode dfs(int[] preorder, int[] start,
+                         int[] inorder, int inStart, int inEnd){
+            if( start[0]>=preorder.length || inStart>inEnd )
                 return null;
+            TreeNode root = new TreeNode(preorder[start[0]]);
+            int inorder_index = inStart;
+            for(int i = inStart; i<=inEnd; i++){
+                if(inorder[i]==preorder[start[0]])
+                    inorder_index = i;
             }
-            TreeNode root = new TreeNode( preorder[preStart] );
-            int rootIndex = 0;
-            for (int i = inStart; i <= inEnd; i++ ){
-                rootIndex = (inorder[i] == root.val)? i: rootIndex;
-            }
-            root.left = helper(preStart+1, inStart, rootIndex-1, preorder, inorder);
-            root.right = helper(preStart + (rootIndex-inStart) +1 , rootIndex+1, inEnd, preorder, inorder);
-            // right sub tree start index in preorder = root index + number of left subtree nodes +1
+            start[0]++;
+            root.left = dfs(preorder, start, inorder, inStart, inorder_index-1);
+            root.right = dfs(preorder, start, inorder, inorder_index+1, inEnd);
             return root;
         }
 }
