1e8988d2e92336957cb32f72daab83769a5e121b
WanglinSong <wsong14@hawk.iit.edu>
2019-09-04 17:41:40 -0500

    house robber

--- Leetcode/dynamic_programming/198_HouseRobber/198_HousRobber.java
+++ Leetcode/dynamic_programming/198_HouseRobber/198_HousRobber.java
@@ -3,10 +3,12 @@ class Solution {
         int n = nums.length;
         if(n==0)
             return 0;
-        int[] dp = new int[n+1];
-        dp[n-1] = nums[n-1];
-        for(int i = n-2; i>=0; i--)
-            dp[i] = Math.max(nums[i]+dp[i+2], dp[i+1]);
+        int[] dp = new int[2];
+        for(int i = n-1; i>=0; i--){
+            int temp = dp[0];
+            dp[0] = Math.max(nums[i]+dp[1], dp[0]);
+            dp[1] = temp;
+        }
         return dp[0];
     }
 }
--- Leetcode/dynamic_programming/198_HouseRobber/Solution.py
+++ Leetcode/dynamic_programming/198_HouseRobber/Solution.py
@@ -13,8 +13,10 @@ class Solution(object):
         if not nums:
             return 0
         n = len(nums)
-        dp = [0]*(n+1)
-        dp[n-1] =nums[-1]
+        dp = [0]*2
+        dp[0] =nums[-1]
         for i in xrange(n-2, -1, -1):
-            dp[i] = max(nums[i]+dp[i+2], dp[i+1])
+            tmp = dp[0]
+            dp[0] = max(nums[i]+dp[1], dp[0])
+            dp[1] = tmp
         return dp[0]
