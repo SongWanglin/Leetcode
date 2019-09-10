42d4de99cf695103228bd91e332e92ab138610ae
WanglinSong <wsong14@hawk.iit.edu>
2019-09-10 17:01:42 -0500

    surrounded regions

--- Leetcode/dfs_recursion_backtracking/130_SurroundedRegions/130_SurroundedRegions.java
+++ Leetcode/dfs_recursion_backtracking/130_SurroundedRegions/130_SurroundedRegions.java
@@ -1,28 +1,35 @@
 class Solution {
     public void solve(char[][] board) {
-        int r = board.length;
-        if (r==0 ) return;
-        int c = board[0].length;
-        for(int i=0; i<r; i++){
-            for (int j=0; j<c; j++){
-                if(i*(r-1-i)!=0 && j*(c-1-j)!=0 ) continue;
-                if (board[i][j] == 'O')
-                    DFS(board, i, j);
-            }
+        if(board==null || board.length==0) return;
+        for(int i = 0; i<board.length; ++i){
+            if(board[i][0]=='O')
+                dfsMarkBorderO(board, i, 0);
+            if(board[i][board[i].length-1]=='O')
+                dfsMarkBorderO(board, i, board[i].length-1);
         }
-        for(int i=0; i<r; i++){
-            for (int j=0; j<c; j++){
-                if(board[i][j] == 'X' ) continue;
-                board[i][j] = (board[i][j]=='x')? 'O': 'X';
+        for(int j = 0; j<board[0].length; ++j){
+            if(board[0][j]=='O')
+                dfsMarkBorderO(board, 0, j);
+            if(board[board.length-1][j]=='O')
+                dfsMarkBorderO(board, board.length-1, j);
+        }
+        for(int i = 0; i<board.length; ++i){
+            for(int j = 0; j<board[i].length; ++j){
+                if( board[i][j]=='X' )
+                    continue;
+                board[i][j] = (board[i][j]=='O')? 'X': 'O';
             }
         }
     }
-    public int DFS(char[][] grid, int i, int j){
-        if ( i<0 || i>=grid.length || j<0 ||j>=grid[0].length ){
-            return 0;
+        private void dfsMarkBorderO(char[][] board,
+                            int i, int j){
+            if( i<0 || j<0 || i>=board.length || j>=board[i].length|| board[i][j]!='O'){
+                return;
+            }
+            board[i][j] = 'x';
+            dfsMarkBorderO(board, i+1, j);
+            dfsMarkBorderO(board, i-1, j);
+            dfsMarkBorderO(board, i, j+1);
+            dfsMarkBorderO(board, i, j-1);
         }
-        if (grid[i][j] == 'x' || grid[i][j]=='X') return 1;
-        grid[i][j] = 'x';
-        return DFS(grid, i-1, j)|DFS(grid, i+1, j)| DFS(grid, i, j-1)|DFS(grid, i, j+1);
-    }
 }
