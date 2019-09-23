edfb2d9e23099ede35215ee4ccb9833c1b3eea12
WanglinSong <wsong14@hawk.iit.edu>
2019-09-23 11:19:12 -0500

    maximum binary tree II

---
+++ Leetcode/tree/998_MaximumBinaryTreeII/Solution.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        if(root==null)
+            return new TreeNode(val);
+        if(root!=null && root.val < val){
+            TreeNode res = new TreeNode(val);
+            res.left = root;
+            return res;
+        }
+        root.right = insertIntoMaxTree(root.right, val);
+        return root;
+    }
+}
