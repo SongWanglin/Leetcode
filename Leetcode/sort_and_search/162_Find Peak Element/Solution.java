0a2c803c541981ede6e6542103f1a36420ea6e86
WanglinSong <wsong14@hawk.iit.edu>
2019-10-08 19:08:48 -0500

    find peak element

--- Leetcode/sort_and_search/162_Find Peak Element/Solution.java
+++ Leetcode/sort_and_search/162_Find Peak Element/Solution.java
@@ -1,14 +1,13 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int start = 0, end = nums.length-1, mid = 0;
-        while(start<end){
-            mid = start+(end-start)/2;
-            if (nums[mid+1]>nums[mid]){
-                start = mid+1;
-            } else {
-                end = mid;
-            }
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[mid + 1])
+                r = mid;
+            else
+                l = mid + 1;
         }
-        return (nums[start]>nums[end])? start: end;
+        return l;
     }
 }
