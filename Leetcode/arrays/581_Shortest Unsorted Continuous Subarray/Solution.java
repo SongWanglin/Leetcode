45078b94de8089fc86300291059ffcef22cfb05b
WanglinSong <wsong14@hawk.iit.edu>
2019-10-11 23:44:39 -0500

    shortest unsorted continuous subarray

--- Leetcode/arrays/581_Shortest Unsorted Continuous Subarray/Solution.java
+++ Leetcode/arrays/581_Shortest Unsorted Continuous Subarray/Solution.java
@@ -1,15 +1,10 @@
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
-        int[] numsCopy = nums.clone();
-        Arrays.sort(numsCopy);
-        int start = numsCopy.length, end = 0;
-        for(int i = 0; i<numsCopy.length; i++){
-            if(numsCopy[i]==nums[i]){
-                continue;
-            }
-            start = Math.min(start, i);
-            end = Math.max(end, i);
-        }
+        int[] copy = nums.clone();
+        Arrays.sort(nums);
+        int start = 0, end = nums.length-1;
+        while(start<=end && nums[start]==copy[start]) start+=1;
+        while(end>=0 && nums[end]==copy[end]) end-=1;
         return (end-start>=0)? end-start+1: 0;
     }
 }
