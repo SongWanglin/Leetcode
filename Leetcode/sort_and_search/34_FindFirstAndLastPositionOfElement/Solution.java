2b4f9e1443097cd0b7525f146472df8174936ffc
WanglinSong <wsong14@hawk.iit.edu>
2019-10-16 00:27:37 -0500

    first and last position of element

--- Leetcode/sort_and_search/34_FindFirstAndLastPositionOfElement/Solution.java
+++ Leetcode/sort_and_search/34_FindFirstAndLastPositionOfElement/Solution.java
@@ -1,31 +1,21 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int n = nums.length, start = 0, end = n, mid;
-        int[] res = new int[2];
-        if(nums.length==0){
-            return new int[]{-1, -1};
-        }
-        do{
+        int start = 0, end = nums.length-1, mid = -1;
+        if(nums.length==0) return new int[]{-1, -1};
+        while(start<=end){
             mid = (start+end)/2;
-            if(nums[mid]==target) break;
-            if( nums[mid]>target ){
-                end = mid;
-            } else{
+            if(nums[mid]==target)
+                break;
+            if(nums[mid]<target){
                 start = mid+1;
+            }else{
+                end = mid-1;
             }
-        }while( start < end );
-        res[0] = (nums[mid]!=target)?-1:mid;
-        res[1] = (nums[mid]!=target)?-1:mid;
-        do {
-            if(res[0]==0 || res[0]== -1) break;
-            if(nums[res[0]]!=nums[res[0]-1]) break;
-            res[0]--;
-        }while( nums[res[0]]==nums[res[0]+1] );
-        do {
-            if(res[1]==nums.length-1 || res[1]==-1) break;
-            if(nums[res[1]]!=nums[res[1]+1]) break;
-            res[1]++;
-        }while( nums[res[1]]==nums[res[1]-1] );
+        }
+        if(nums[mid]!=target) return new int[]{-1, -1};
+        int[] res = new int[]{mid, mid};
+        while(res[0]>0 && nums[res[0]]==nums[res[0]-1]){res[0]--;}
+        while(res[1]<nums.length-1 && nums[res[1]]==nums[res[1]+1]){res[1]++;}
         return res;
     }
 }
